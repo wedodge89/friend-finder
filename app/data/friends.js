216b8080269a08d50ec7ed4620835779eeab32ab
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-01-09 18:30:29 -0500

    changed survey questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Elliott",
+    "photo":"../../elliott-img.jpg",
     "scores":[
         5,
         1,
